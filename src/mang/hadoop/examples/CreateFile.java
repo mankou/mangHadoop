40a14fd05b1ce06cd4a7fc388d27cf2b681c9005
dorm_mang <ningma003@gmail.com>
2012-11-06 11:43:57 +0800

    createFile.java中添加了执行成功说明信息

--- src/mang/hadoop/examples/CreateFile.java
+++ src/mang/hadoop/examples/CreateFile.java
@@ -6,29 +6,38 @@
  * 		 参考二：http://blog.csdn.net/nodie/article/details/6386586
  * */
 package mang.hadoop.examples;
+
 import java.util.Date;
 
-import org.apache.hadoop.conf.*;  
-import org.apache.hadoop.fs.FileSystem;  
-import org.apache.hadoop.hdfs.*;  
-import org.apache.hadoop.fs.*;  
-public class CreateFile {  
-public static void main(String[] args) throws Exception{  
-       String str = "Hello world";  
-       Configuration configuration = new Configuration();  
-       FileSystem fs = FileSystem.get(configuration);  
-       /*
-        * 原来参考一上是用java.io的，执行后虽然新建了文件但没有写入内容。
-        * 后来看到参考二可以用hdfs的io，就用hdfs的io了
-        * */
-//       java.io.OutputStream out = fs.create(new Path("/user/root/input/helloworld"));  
-       FSDataOutputStream dos = fs.create(new Path("/user/root/input/helloworld")); 
-       String utf8 = "UTF-8";
-       dos.write("/r/n".getBytes(utf8));
-       dos.write("//ceshi//".getBytes(utf8));
-       dos.write("/r/n".getBytes(utf8));
-       Date dat = new Date();
-       dos.write(dat.toString().getBytes());
-       dos.close();  
-    }  
-    }  
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.hdfs.*;
+import org.apache.hadoop.fs.*;
+
+public class CreateFile {
+	public static void main(String[] args) throws Exception {
+		try {
+			String str = "Hello world";
+			Configuration configuration = new Configuration();
+			FileSystem fs = FileSystem.get(configuration);
+			/*
+			 * 原来参考一上是用java.io的，执行后虽然新建了文件但没有写入内容。 后来看到参考二可以用hdfs的io，就用hdfs的io了
+			 */
+			// java.io.OutputStream out = fs.create(new
+			// Path("/user/root/input/helloworld"));
+			FSDataOutputStream dos = fs.create(new Path(
+					"/user/root/input/helloworld"));
+			String utf8 = "UTF-8";
+			dos.write("/r/n".getBytes(utf8));
+			dos.write("//ceshi//".getBytes(utf8));
+			dos.write("/r/n".getBytes(utf8));
+			Date dat = new Date();
+			dos.write(dat.toString().getBytes());
+			dos.close();
+			System.out.println("create file success");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+
+	}
+}

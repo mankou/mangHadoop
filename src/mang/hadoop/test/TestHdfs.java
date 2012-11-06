37d7aa25b55b11a8da4a991070b367a158dfb440
mang_lab <ningma003@gmail.com>
2012-11-06 22:52:03 +0800

    lab重新clone了hadoo库，现在把以前没有加入版本库的两个文件加进来

---
+++ src/mang/hadoop/test/TestHdfs.java
@@ -0,0 +1,51 @@
+/*
+ * 说明：用于在HDFS进行文件操作的，但没有成功，这里先不删除
+ * */
+package mang.hadoop.test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+
+
+
+public class TestHdfs {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestHdfs test=new TestHdfs();
+		Configuration conf = new Configuration();
+		String FileName=new String("input");
+		test.GetFileBolckHost(conf, FileName);
+
+	}
+	public List<String[]> GetFileBolckHost(Configuration conf, String FileName) {  
+        try {  
+            List<String[]> list = new ArrayList<String[]>();  
+            FileSystem hdfs = FileSystem.get(conf);  
+            Path path = new Path(FileName);  
+            FileStatus fileStatus = hdfs.getFileStatus(path);  
+  
+            BlockLocation[] blkLocations = hdfs.getFileBlockLocations(  
+                    fileStatus, 0, fileStatus.getLen());  
+  
+            int blkCount = blkLocations.length;  
+            for (int i = 0; i < blkCount; i++) {  
+                String[] hosts = blkLocations[i].getHosts();  
+                System.out.println(hosts);
+                list.add(hosts);  
+            }  
+            return list;  
+        } catch (IOException e) {  
+            e.printStackTrace();  
+        }  
+        return null;  
+    }
+
+}
